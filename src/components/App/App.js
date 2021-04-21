2898a466e3283e0985282ec019c5a98237d3956d
Boaz <42014936+boazmenard@users.noreply.github.com>
2021-04-21 13:17:40 -0400

    implemented fetch api to consume the yelp api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
+import { Yelp } from "../../util/Yelp";
 
 const business = {
     imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
@@ -22,14 +23,30 @@ const searchYelp = (term, location, sortBy) => {
     console.log(`Searching yelp with ${term}, ${location}, ${sortBy}`);
 }
 
-function App() {
-  return (
-      <div className="App">
-        <h1>ravenous</h1>
-        <SearchBar searchYelp={searchYelp}/>
-        <BusinessList business={businesses} />
-      </div>
-  );
-}
+export class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            businesses: []
+        };
+        this.searchYelp = this.searchYelp.bind(this);
+    }
 
-export default App;
+    searchYelp(term, location, sortBy) {
+        Yelp.search(term, location, sortBy).then(businesses => {
+            this.setState({
+                businesses: businesses
+            })
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>ravenous</h1>
+                <SearchBar searchYelp={this.searchYelp}/>
+                <BusinessList business={businesses} />
+            </div>
+        );
+    }
+}
